a1ba38cabf9372ff6de531bc6494322b4a1076a2
Vansh Goel <goelvansh149@gmail.com>
2026-06-07 22:05:33 +0530

    Log MONGO_URI when connecting to the database

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { MONGO_URI } from './env.js';
 
 export async function connectDB() {
+  console.log("URI:", JSON.stringify(MONGO_URI));
   try {
     await mongoose.connect(MONGO_URI, {
       autoIndex: true,
